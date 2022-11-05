6ee1bfee331cb97c8133421d5c28a4accab52975
Umar <umargithubacc@gmail.com>
2022-11-06 02:06:44 +0600

    1

---
+++ main.js
@@ -0,0 +1,31 @@
+$('.my-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.review__carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2.7
+        },
+
+    }
+})
